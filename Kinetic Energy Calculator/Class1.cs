14fcbdb409e4578e147fc0b78f1de011a5f7fd37
Roberto <Roberto@DESKTOP-1818MJE>
2024-02-19 20:45:08 -0600

    Agregar archivos de proyecto.

---
+++ Kinetic Energy Calculator/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinetic_Energy_Calculator
+{
+    public class KineticEnergyCalculator
+    {
+        
+            private double mass;
+            private double velocity;
+
+            public KineticEnergyCalculator(double mass, double velocity)
+            {
+                this.mass = mass;
+                this.velocity = velocity;
+            }
+
+            public double CalculateKineticEnergy()
+            {
+                return 0.5 * mass * Math.Pow(velocity, 2);
+            }
+        
+    }
+}
---
+++ Kinetic Energy Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using Kinetic_Energy_Calculator;
+
+Console.WriteLine("Kinetic Energy Calculator");
+
+
+Console.Write("Enter the mass of the object (kg): ");
+double mass = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the velocity of the object (m/s): ");
+double velocity = Convert.ToDouble(Console.ReadLine());
+
+KineticEnergyCalculator calculator = new KineticEnergyCalculator(mass, velocity);
+double kineticEnergy = calculator.CalculateKineticEnergy();
+
+Console.WriteLine($"The kinetic energy is: {kineticEnergy} Joules");
+
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
